de83a640cba930955f3268a4d51663c2e8d69886
nazar-lynovetsky <nazarlynovetsky@gmail.com>
2024-02-18 11:57:01 +0200

    A little refactoring.

--- public/scripts/components/navigation.mjs
+++ public/scripts/components/navigation.mjs
@@ -1,29 +1,29 @@
-var nav = document.querySelector( '#nav' )
-var button = nav.querySelector( 'button' )
+var nav = document.querySelector('#nav')
+var button = nav.querySelector('button')
 
 /**
  * Event Listeners
  */
 
-button.addEventListener( 'click', e => {
-  setAriaExpanded( button, !getAriaExpanded( button ) )
-} )
+button.addEventListener('click', e => {
+  setAriaExpanded(button, !getAriaExpanded(button))
+})
 
-nav.addEventListener( 'keyup', e => {
-  if ( e.code === 'Escape' ) {
-    setAriaExpanded( button, false )
+nav.addEventListener('keyup', e => {
+  if (e.code === 'Escape') {
+    setAriaExpanded(button, false)
   }
-} )
+})
 
 /**
  * Methods
  */
 
-function getAriaExpanded( el ) {
-  return el.getAttribute( 'aria-expanded' ) === 'true'
+function getAriaExpanded(el) {
+  return el.getAttribute('aria-expanded') === 'true'
 }
 
-function setAriaExpanded( el, val ) {
-  el.setAttribute( 'aria-expanded', val )
+function setAriaExpanded(el, val) {
+  el.setAttribute('aria-expanded', val)
 }
 
